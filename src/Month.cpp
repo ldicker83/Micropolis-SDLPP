979d8214d6e5919b654f86b73bfb4c8ff78af92b
Leeor Dicker <leeor.net@gmail.com>
2026-04-20 18:11:12 -0400

    Load month strings from strings.json

--- src/Month.cpp
+++ src/Month.cpp
@@ -1,27 +1,69 @@
 #include "Month.h"
 
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+
+using json = nlohmann::json;
+
 
 namespace
 {
-    const std::string MonthTable[12] =
-    {
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
-    };
-}
+	bool MonthStringsLoaded{ false };
 
+    std::string MonthStrings[12];
+
+    std::ifstream loadStringsFile()
+    {
+        std::ifstream stringsFile("res/strings.json");
+        if (!stringsFile.is_open())
+        {
+            throw std::runtime_error("Could not open 'res/strings.json'");
+        }
+        return stringsFile;
+	}
+
+
+    auto monthsArrayFromJson(const json& data)
+    {
+        if (!data.contains("strings") || !data["strings"].contains("months"))
+        {
+            throw std::runtime_error("Strings file missing 'strings.months' structure");
+        }
+        
+        const auto& monthsArray = data["strings"]["months"];
+        
+        if (!monthsArray.is_array() || monthsArray.size() != 12)
+        {
+            throw std::runtime_error("Expected 12 months in strings file");
+        }
+
+        return monthsArray;
+	}
+
+
+    void loadMonthStrings()
+    {
+        json data = json::parse(loadStringsFile());
+
+		auto months = monthsArrayFromJson(data);
+
+        for (size_t i = 0; i < 12; ++i)
+        {
+            MonthStrings[i] = months[i].get<std::string>();
+        }
+
+        MonthStringsLoaded = true;
+    }
+}
 
 const std::string& Month::toString(Month::Enum month)
 {
-    return MonthTable[static_cast<int>(month)];
+	const int index = static_cast<int>(month);
+    if (!MonthStringsLoaded)
+    {
+        loadMonthStrings();
+    }
+
+    return MonthStrings[index];
 }
