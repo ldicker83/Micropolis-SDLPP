44eed84e019dd2f23bdbd56e032f28c03a9cd37b
Leeor Dicker <leeor.net@gmail.com>
2026-04-20 21:14:11 -0400

    Update copyright notices across entire project

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-// This file is part of Micropolis-SDL2PP
-// Micropolis-SDL2PP is based on Micropolis
+// This file is part of Micropolis-SDLPP
+// Micropolis-SDLPP is based on Micropolis
 //
 // Copyright © 2022 - 2026 Leeor Dicker
 //
 // Portions Copyright © 1989-2007 Electronic Arts Inc.
 //
-// Micropolis-SDL2PP is free software; you can redistribute it and/or modify
+// Micropolis-SDLPP is free software; you can redistribute it and/or modify
 // it under the terms of the GNU GPLv3, with additional terms. See the README
 // file, included in this distribution, for details.
 #include "main.h"
@@ -1084,10 +1084,10 @@ int main(int argc, char* argv[])
     }
     catch(const std::exception& e)
     {
-        std::string message(std::string(e.what()) + "\n\nMicropolis-SDL2PP will now close.");
+        std::string message(std::string(e.what()) + "\n\nMicropolis-SDLPP will now close.");
         
         #if defined(_WIN32)
-        MessageBoxA(nullptr, message.c_str(), "Micropolis-SDL2PP", MB_ICONERROR | MB_OK);
+        MessageBoxA(nullptr, message.c_str(), "Micropolis-SDLPP", MB_ICONERROR | MB_OK);
         #else
         std::cout << message << std::endl;
         #endif
